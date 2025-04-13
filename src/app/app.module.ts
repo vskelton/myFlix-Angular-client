3a0ca27b45ee28856f810d55c10662e052021b5e
vskelton <vskelton25@gmail.com>
2025-04-13 19:11:08 -0400

    Create user profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 @NgModule({
@@ -26,6 +27,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     UserLoginFormComponent,
     MovieCardComponent,
     WelcomePageComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
